98ed57581acf0dce0e95cfc86db29a39dd62b2b3
Mathias <msmendozaa@unac.edu.pe>
2025-09-03 10:30:51 -0500

    El segundo commit

--- Practica01/Program.cs
+++ Practica01/Program.cs
@@ -10,6 +10,19 @@ namespace Practica01
     {
         static void Main(string[] args)
         {
+            Console.Write("Hola, ¿Cual es tu nombre?: ") ;
+            string nombre = Console.ReadLine() ;
+
+            Console.Write("¿Cual es tu edad?: ");
+            int edad = Convert.ToInt32(Console.ReadLine()) ;
+
+            Console.Write("¿Cual es tu estatura?: ");
+            decimal estatura = Convert.ToDecimal(Console.ReadLine()) ;
+
+            Console.Write($"Mi nombre es {nombre}, mi edad es {edad} y mi estatura es {estatura:N2} ");
+
+
+
         }
     }
 }
